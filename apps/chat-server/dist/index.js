579d67a7d22a5f9a1c52cf5fe8b5265fbb2f0e33
Muneeb Rehman <muneebrehman1992@gmail.com>
2022-11-01 02:44:57 +0500

    change type to commonjs

--- apps/chat-server/dist/index.js
+++ apps/chat-server/dist/index.js
@@ -1,10 +1,35 @@
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import * as dotenv from 'dotenv';
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const dotenv = __importStar(require("dotenv"));
 dotenv.config();
-const httpServer = createServer();
+const httpServer = (0, http_1.createServer)();
 const port = process.env.PORT || 5500;
-const io = new Server(httpServer, {
+const io = new socket_io_1.Server(httpServer, {
     cors: {
         origin: "*",
     }
@@ -26,4 +51,3 @@ io.on('connection', (socket) => {
 httpServer.listen(port, () => {
     console.log(`⚡️[chat-server]: running`);
 });
-export default httpServer;
