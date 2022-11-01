ac004f8b72b8ff94807df9b3dd370c2034401447
Muneeb Rehman <muneebrehman1992@gmail.com>
2022-11-01 18:06:06 +0500

    change button text

--- apps/chat-app/src/core/setUserDialog.tsx
+++ apps/chat-app/src/core/setUserDialog.tsx
@@ -82,7 +82,7 @@ export default function SetUserDialog(props: Props) {
                                             className="w-full inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                                             onClick={saveHandler}
                                         >
-                                            Start Chatting
+                                            Join
                                         </button>
                                     </div>
                                 </Dialog.Panel>
